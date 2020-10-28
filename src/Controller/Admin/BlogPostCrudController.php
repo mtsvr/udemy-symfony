601bb185a16c58a8835d06e3e99e0b9ce3f377b6
Matias Vera <matias.veraaravena@gmail.com>
2020-10-28 17:59:10 -0300

    working easy admin

---
+++ src/Controller/Admin/BlogPostCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\BlogPost;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+
+class BlogPostCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return BlogPost::class;
+    }
+
+    public function configureFields(string $pageName): iterable {
+        return [
+            TextField::new('title'),
+            TextField::new('slug'),
+            TextField::new('author'),
+            TextEditorField::new('content'),
+            DateTimeField::new('published'),
+        ];
+    }
+}
